7a699f028f4e4c897fd03d0de0e62e0eff0a553f
anthocodes99 <anthocodes99@users.noreply.github.com>
2024-03-26 18:13:48 +0800

    fix: :lock: SECRET_KEY to rely on environment variable instead
    for added security

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -1,7 +1,13 @@
+import os
+
 from .base import *
+
+# NOTE: for sensitive settings, please use environment variables!
+# you may also employ Flask's instance folder method instead
+# https://flask.palletsprojects.com/en/3.0.x/config/#instance-folders
 
 # ensure that debug mode is not on
 DEBUG = False
 
 # different secret key in production for increased security
-SECRET_KEY = b'e^penxx=uj2zq1p7ao=hale(#fu%iq*)cnfewp*29_eovw$kwc'
+SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', b'e^penxx=uj2zq1p7ao=hale(#fu%iq*)cnfewp*29_eovw$kwc')
